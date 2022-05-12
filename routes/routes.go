b5de3d6beaad2a229651593343678d19f204819c
Douglas Lira <douglas.vclira@gmail.com>
2022-05-12 13:12:18 -0300

    incluindo middleware, integração com frontend e configuração de CORS

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"github.com/douglira/alura-golang-api-rest.git/controllers"
+	"github.com/douglira/alura-golang-api-rest.git/middlewares"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 
 	"log"
@@ -11,10 +13,11 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	subRouterAPI := r.PathPrefix("/api").Subrouter()
+	subRouterAPI.Use(middlewares.SetApplicationJsonContentType)
 	subRouterAPI.HandleFunc("/personalities", controllers.AllPersonalities).Methods(http.MethodGet)
 	subRouterAPI.HandleFunc("/personalities", controllers.RegisterPersonality).Methods(http.MethodPost)
 	subRouterAPI.HandleFunc("/personalities/{id}", controllers.FindPersonality).Methods(http.MethodGet)
 	subRouterAPI.HandleFunc("/personalities/{id}", controllers.UpdatePersonality).Methods(http.MethodPut)
 	subRouterAPI.HandleFunc("/personalities/{id}", controllers.DeletePersonality).Methods(http.MethodDelete)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
