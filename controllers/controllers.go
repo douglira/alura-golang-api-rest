12d933c16c7f5b8c4fa8817ab239fc011e6409c2
Douglas Lira <douglas.vclira@gmail.com>
2022-05-12 11:46:16 -0300

    exclusão e atualização dos dados

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/douglira/alura-golang-api-rest.git/database"
 	"github.com/douglira/alura-golang-api-rest.git/models"
 	"github.com/gorilla/mux"
+	"gorm.io/gorm/clause"
 
 	"encoding/json"
 	"net/http"
@@ -23,5 +26,31 @@ func FindPersonality(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w)
 		return
 	}
+	json.NewEncoder(w).Encode(personality)
+}
+
+func RegisterPersonality(w http.ResponseWriter, r *http.Request) {
+	var personality models.Personality
+	json.NewDecoder(r.Body).Decode(&personality)
+	database.DB.Select("Name", "History").Create(&personality)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func UpdatePersonality(w http.ResponseWriter, r *http.Request) {
+	var personality models.Personality
+	personalityId, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	json.NewDecoder(r.Body).Decode(&personality)
+	personality.Id = personalityId
+	database.DB.Clauses(clause.Returning{}).Updates(&personality)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func DeletePersonality(w http.ResponseWriter, r *http.Request) {
+	personalityId := mux.Vars(r)["id"]
+	var personality models.Personality
+	database.DB.Clauses(clause.Returning{}).Delete(&personality, personalityId)
 	json.NewEncoder(w).Encode(personality)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,10 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	subRouterAPI := r.PathPrefix("/api").Subrouter()
-	subRouterAPI.HandleFunc("/personalities", controllers.AllPersonalities).Methods("GET")
-	subRouterAPI.HandleFunc("/personalities/{id}", controllers.FindPersonality).Methods("GET")
+	subRouterAPI.HandleFunc("/personalities", controllers.AllPersonalities).Methods(http.MethodGet)
+	subRouterAPI.HandleFunc("/personalities", controllers.RegisterPersonality).Methods(http.MethodPost)
+	subRouterAPI.HandleFunc("/personalities/{id}", controllers.FindPersonality).Methods(http.MethodGet)
+	subRouterAPI.HandleFunc("/personalities/{id}", controllers.UpdatePersonality).Methods(http.MethodPut)
+	subRouterAPI.HandleFunc("/personalities/{id}", controllers.DeletePersonality).Methods(http.MethodDelete)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
